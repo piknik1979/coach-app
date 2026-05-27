57160344dd514871e4b9c7dee6ee19a87681b763
piknik1979 <tpikniczka@gmail.com>
2026-05-27 17:05:33 +0100

    bubel

---
+++ config.js
@@ -0,0 +1,4 @@
+const CONFIG = {
+    SB_URL: "https://icodelhzrzmdlrpgsygh.supabase.co",
+    SB_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imljb2RlbGh6cnptZGxycGdzeWdoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYyNTM2OTIsImV4cCI6MjA5MTgyOTY5Mn0.1CIOeHwKxKMBwX2Lee9OA0LYFpVdDevealH6l6PlkC4"
+};
